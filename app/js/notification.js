64354f050ec4800dc4bc70786efdbf5dafba5bc6
AkmalKurbanov <250296a@gmail.com>
2024-06-03 14:39:58 +0600

    end

--- app/js/notification.js
+++ app/js/notification.js
@@ -3,12 +3,21 @@ $(document).ready(function () {
   $(".notification-js").on("click", function (event) {
     event.stopPropagation(); // Остановка всплытия события клика
     $(".notification__dropdown").toggleClass("active");
+
+    if (window.matchMedia("(max-width: 575px)").matches) {
+      $("body").toggleClass("no-scroll");
+    } else {
+    }
   });
 
   // Удаление класса у второго элемента при клике вне него
   $(document).on("click", function (event) {
     if (!$(event.target).closest(".notification__dropdown").length) {
       $(".notification__dropdown").removeClass("active");
+      if (window.matchMedia("(max-width: 575px)").matches) {
+        $("body").removeClass("no-scroll");
+      } else {
+      }
     }
   });
 
--- app/js/popup.js
+++ app/js/popup.js
@@ -1,6 +1,8 @@
 $(".trade-js").on('click', function(){
   $(".popup-js").addClass('active');
+  $("body").addClass('no-scroll');
 });
 $(".popup-cancel-js").on("click", function () {
   $(".popup-js").removeClass("active");
+  $("body").removeClass('no-scroll');
 });
